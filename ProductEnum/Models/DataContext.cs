dfccb57a2eaa296a2e6bcbe25aa45c69138d2980
joseandresh <a2019112027@estudiantes.upsa.edu.bo>
2020-09-12 11:58:09 -0400

    Controllador creado.

---
+++ ProductEnum/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace ProductEnum.Models
+{
+	public class DataContext: DbContext
+	{
+		public DataContext(): base("DefaultConnection")
+		{
+
+		}
+
+		public System.Data.Entity.DbSet<ProductEnum.Models.Product> Products { get; set; }
+	}
+}
---
+++ ProductEnum/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductEnum.Models
+{
+	public enum CategoryType
+	{
+		Electronicos = 10,
+		Alimenticios = 20,
+		Moda = 30,
+		Belleza = 40,
+		Automotriz = 60,
+		Libros = 60
+	};
+
+	public class Product
+	{
+		[Key]
+		public int ProductId { get; set; }
+
+		[Display(Name = "Nombre del producto")]
+		[Required(ErrorMessage = "Debe ingresar el nombre del producto")]
+		public string Name { get; set; }
+
+		[Display(Name = "Categoria")]
+		[Required(ErrorMessage = "Debe ingresar la categoría del producto")]
+		public CategoryType Category { get; set; }
+	}
+}
